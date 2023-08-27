3212aa3bde23db1239ae7559d3f9d5640e02a6dd
Mark Baggett <mbagget1@utk.edu>
2023-08-27 17:45:09 -0400

    Init ArchivalObject.

--- archiveswurm/__init__.py
+++ archiveswurm/__init__.py
@@ -1,5 +1,6 @@
 from .accession import Accession
 from .archivesspace import ArchivesSpace
+from .archivalobject import ArchivalObject
 from .digitalobject import DigitalObject
 from .repository import Repository
 from .resource import Resource
