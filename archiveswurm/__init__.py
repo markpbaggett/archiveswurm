9a2d08dabd0a24948c0c14b7e6f9ab272e45642a
Mark Baggett <mbagget1@utk.edu>
2023-08-27 17:40:24 -0400

    Init resource.

--- archiveswurm/__init__.py
+++ archiveswurm/__init__.py
@@ -2,3 +2,4 @@ from .accession import Accession
 from .archivesspace import ArchivesSpace
 from .digitalobject import DigitalObject
 from .repository import Repository
+from .resource import Resource
