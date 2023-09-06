668dcd5df57cad323000af85ebbb9f4a8774467b
Mark Baggett <mbagget1@utk.edu>
2023-09-06 09:00:16 -0400

    Init DigitalObjectComponent.

--- archiveswurm/__init__.py
+++ archiveswurm/__init__.py
@@ -1,6 +1,7 @@
 from .accession import Accession
 from .archivesspace import ArchivesSpace
 from .archivalobject import ArchivalObject
+from .digitalcomponent import DigitalObjectComponent
 from .digitalobject import DigitalObject
 from .event import Event
 from .repository import Repository
