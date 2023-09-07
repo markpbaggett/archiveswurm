e10e145e34074b4a56d9d1c5b4c79d5c571c8a24
Mark Baggett <mbagget1@utk.edu>
2023-09-07 10:43:38 -0400

    Create components and children of components.

--- archiveswurm/digitalcomponent/digitalcomponent.py
+++ archiveswurm/digitalcomponent/digitalcomponent.py
@@ -61,12 +61,59 @@ class DigitalObjectComponent(ArchivesSpace):
             dict: The digital object component as a dict.
 
         Examples:
-            >>> DigitalObjectComponent().get(2, 2)
-            {'lock_version': 0, 'jsonmodel_type': 'digital_object_component', 'suppressed': False, 'display_string': 'test', 'uri': '/repositories/2/digital_object_components/2', 'created_by': 'admin', 'last_modified_by': 'admin', 'create_time': '2021-04-08T14:34:37Z', 'system_mtime': '2021-04-08T14:34:37Z', 'user_mtime': '2021-04-08T14:34:37Z', 'digital_object': {'ref': '/repositories/2/digital_objects/2', 'display_string': 'test'}, 'file_versions': []}
+            >>> DigitalObjectComponent().get(2, 1)
+            {'lock_version': 0, 'position': 0, 'publish': False, 'display_string': 'Chronicling COVID-19: AIP', 'label': 'Chronicling COVID-19: AIP', 'created_by': 'mark', 'last_modified_by': 'mark', 'create_time': '2023-04-16T14:43:48Z', 'system_mtime': '2023-04-16T14:43:48Z', 'user_mtime': '2023-04-16T14:43:48Z', 'suppressed': False, 'is_slug_auto': False, 'jsonmodel_type': 'digital_object_component', 'external_ids': [], 'subjects': [], 'linked_events': [], 'extents': [], 'lang_materials': [], 'dates': [], 'external_documents': [], 'rights_statements': [], 'linked_agents': [], 'file_versions': [{'lock_version': 0, 'file_uri': 'https://n2t.net/ark:/87290/v8cr5rh5', 'publish': False, 'created_by': 'mark', 'last_modified_by': 'mark', 'create_time': '2023-04-16T14:43:48Z', 'system_mtime': '2023-04-16T14:43:48Z', 'user_mtime': '2023-04-16T14:43:48Z', 'xlink_actuate_attribute': 'onRequest', 'xlink_show_attribute': 'new', 'jsonmodel_type': 'file_version', 'is_representative': False, 'identifier': '11'}], 'notes': [], 'uri': '/repositories/2/digital_object_components/1', 'repository': {'ref': '/repositories/2'}, 'digital_object': {'ref': '/repositories/2/digital_objects/11'}, 'has_unpublished_ancestor': True}
 
         """
         r = requests.get(
             url=f"{self.base_url}/repositories/{repo_id}/digital_object_components/{digital_object_component_id}",
             headers=self.headers,
+        )
+        return r.json()
+
+    def create(self, digital_object, title, repo_id, position=0, specified_properties={}, file_versions=[], publish=False, parent=None):
+        """Create a Digital Object Component.
+
+        Args:
+            digital_object (int): The id of the parent Digital Object.
+            title (str): The title of the Digital Object Component.
+            repo_id (int): The id of the repository you are querying and the id of the repository to which the digital object belongs.
+            position (int): The position of the Digital Object Component in the parent Digital Object. Defaults to 0.
+            specified_properties (dict): A dict of properties to add to the Digital Object Component.
+            file_versions (list): A list of file versions to add to the Digital Object Component.
+            publish (bool): Whether or not to publish the Digital Object Component. Defaults to False.
+            parent (int): The id of the parent Digital Object Component if it belongs to one. Defaults to None.
+
+        Returns:
+            dict: The Digital Object Component as a dict.
+
+        Examples:
+            >>> DigitalObjectComponent().create(11, "Test Digital Object Component", 2)
+            {'status': 'Created', 'id': 3, 'lock_version': 0, 'stale': True, 'uri': '/repositories/2/digital_object_components/3', 'warnings': []}
+
+        """
+        initial_data = {
+            "jsonmodel_type": "digital_object_component",
+            "is_slug_auto": False,
+            "publish": publish,
+            "position": position,
+            "title": title,
+            "label": title,
+            "digital_object": {
+                "ref": f'/repositories/{repo_id}/digital_objects/{digital_object}'
+            },
+        }
+        for key, value in specified_properties.items():
+            initial_data[key] = value
+        for file_version in file_versions:
+            initial_data["file_versions"].append(file_version)
+        if parent:
+            initial_data["parent"] = {
+                "ref": f"/repositories/{repo_id}/digital_object_components/{parent}"
+            }
+        r = requests.post(
+            url=f"{self.base_url}/repositories/{repo_id}/digital_object_components",
+            headers=self.headers,
+            data=json.dumps(initial_data),
         )
         return r.json()
